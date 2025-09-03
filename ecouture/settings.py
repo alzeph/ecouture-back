1c6c08bbf4c7849c3da9ab009aac34a283650b99
= <=>
2025-09-03 16:50:16 +0000

    Correction de is_owner

--- ecouture/settings.py
+++ ecouture/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'ecouture.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
